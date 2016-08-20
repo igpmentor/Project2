c0408125d7db8fc344cd16130fd905ef459f4795
vel <vel@velmurugans-mbp>
2016-08-20 08:22:07 +0530

    First file from eclipse

---
+++ Project2/src/igp/project2/SecondExample.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package igp.project2;
+
+/**
+ * @author vel
+ *
+ */
+public class SecondExample {
+
+	/**
+	 * 
+	 */
+	public SecondExample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// this is sample #2
+	}
+
+}
